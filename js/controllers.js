5e042ecd59d796a9538cf4d934f17b14d7a3143c
IsabelleRey <isabellrey@gmail.com>
2015-04-08 14:35:29 -0700

    add a server with node

--- js/controllers.js
+++ js/controllers.js
@@ -5,12 +5,16 @@ var ctrls = angular.module('dabbble.controllers', []);
 ctrls.controller('AppCtrl', function ($scope, $http) {
 });
 
+
+// Controls the list
+
 ctrls.controller('ShotListCtrl', function ($scope, PagedResult, $routeParams) {
   var list = $routeParams.list;
 
   $scope.list = new PagedResult('shots', list, 'shots').loadNextPage();
 
 });
+
 
 ctrls.controller('ShotCtrl', function ($scope, $http, $routeParams, dribbble, PagedResult) {
   var id = $routeParams.id;
